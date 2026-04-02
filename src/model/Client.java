6bfb07f897124e1fd28bb725b08e0c523d07e588
diarradia1977-hub <diarra2.dia@ucad.edu.sn>
2026-04-02 13:58:26 +0000

    Fondations-et-bases-de0donnees

---
+++ src/model/Client.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Client {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private String adresse;
+
+    // Constructeurs, Getters et Setters (Encapsulation)
+    public Client() {}
+    public Client(int id, String nom, String prenom, String telephone, String adresse) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+    }
+    
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+    public String getAdresse() { return adresse; }
+    public void setAdresse(String adresse) { this.adresse = adresse; }
+}
