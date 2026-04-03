8acbdf0801fc0f984983d9d8c35cb20089281b48
Dieynaba BALDE <dieynababalde36@gmail.com>
2026-04-03 01:44:45 +0000

    Confort user

--- src/model/Compte.java
+++ src/model/Compte.java
@@ -7,6 +7,7 @@ public class Compte {
     private Client client;
 
     public Compte() {}
+
     public Compte(String numeroCompte, double solde, Client client) {
         this.numeroCompte = numeroCompte;
         this.solde = solde;
@@ -15,10 +16,21 @@ public class Compte {
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
+
     public String getNumeroCompte() { return numeroCompte; }
     public void setNumeroCompte(String numeroCompte) { this.numeroCompte = numeroCompte; }
+
     public double getSolde() { return solde; }
     public void setSolde(double solde) { this.solde = solde; }
+
     public Client getClient() { return client; }
     public void setClient(Client client) { this.client = client; }
+
+    @Override
+    public String toString() {
+        return String.format("Compte{id=%d, numero='%s', solde=%.3f FCFA, client=%d }",
+            id, numeroCompte, solde, client
+        );
+    }
+
 }
